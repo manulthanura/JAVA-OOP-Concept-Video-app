1eb773952ffb725cb6852437218ea31fd8241d77
Manul Thanura <manulkarunarathna@gmail.com>
2022-11-21 05:43:19 +0530

    update

--- Users.java
+++ Users.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 public class Users {
-    String userName;
-    int userRating;
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Username: ");
+        String user = input.nextLine();
+        System.out.print("Enter Password: ");
+        String pass = input.nextLine();
+        if (user.equalsIgnoreCase("jas") && pass.equals("1224")) {
+            System.out.print("\nWelcome! " + user);
+        } else {
+            System.out.print("You don't have access!");
+        }
+        input.close();
     }
-    
 }
